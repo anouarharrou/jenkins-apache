623323ebe26a20c6ab76f55afb208c4554c97acc
anouarharrou <anouar_-_harrou@outlook.fr>
2024-01-29 10:29:50 +0100

    Update Functions.sh

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -35,7 +35,7 @@ function InstallOnUbuntu() {
     curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
     echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
     sudo apt update -y
-    sudo apt install jenkins -y
+    sudo apt install jenkins=2.401.1 -y
     sudo systemctl start jenkins && sudo systemctl enable jenkins
 
 
